2e9d3d896ee40bfad6e538a7740ff11c5946b4e4
Niclas Moeslund Overby <nimoov@prozum.dk>
2017-04-19 10:31:54 +0200

    Formalize naming of concepts

--- gen-ks-files.sh
+++ gen-ks-files.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
-
-DISTRO="centos7"
-SUB_VARS='$IP:$NETMASK:$GATEWAY:$NAMESERVERS:$HOSTNAME'
-KS_IN_DIR="$PWD/ks"
+TEMPLATE_DIR="$PWD/templates"
+INSTANCE_DIR="$PWD/instances"
+USER_DIR="$PWD/users"
 KS_OUT_DIR="$PWD/tmp/ks"
 
-USERS=$(ls users)
-SERVER_IDS=$(ls servers)
+USERS=$(ls $USER_DIR)
+INSTANCE_IDS=$(ls $INSTANCE_DIR)
+
+SUB_VARS='$IP:$DISTRO:$NETMASK:$GATEWAY:$NAMESERVERS:$HOSTNAME'
 
 # Make kickstart output directory
 mkdir -p $KS_OUT_DIR
 
-for ID in $SERVER_IDS
+# For each instance
+for ID in $INSTANCE_IDS
 do
     # Load settings
-    . "servers/$ID"
-    KS_FILE_IN="$KS_IN_DIR/$DISTRO-$TYPE.cfg.in"
+    . "$INSTANCE_DIR/$ID"
+    KS_FILE_IN="$TEMPLATE_DIR/$DISTRO-$TYPE.cfg.in"
     KS_FILE="$KS_OUT_DIR/$DISTRO-$ID.cfg"
     
     # Generate variable settings
@@ -51,17 +53,17 @@ EOF
     do
         echo "mkdir -m0700 /home/$USER/.ssh/" >> $KS_FILE
         echo "cat <<EOF > /home/$USER/.ssh/authorized_keys" >> $KS_FILE
-        cat "users/$USER" >> $KS_FILE
+        cat "$USER_DIR/$USER" >> $KS_FILE
         echo "EOF" >> $KS_FILE
         echo "cat <<EOF >> /root/.ssh/authorized_keys" >> $KS_FILE
-        cat "users/$USER" >> $KS_FILE
+        cat "$USER_DIR/$USER" >> $KS_FILE
         echo "EOF" >> $KS_FILE
         echo "chmod 0600 /home/$USER/.ssh/authorized_keys" >> $KS_FILE
         echo "chown -R $USER:$USER /home/$USER/.ssh/" >> $KS_FILE
     done
 
     # Cat ks/$DISTRO-$TYPE.sh to post script
-    SCRIPT_FILE="$KS_IN_DIR/$DISTRO-$TYPE.sh"
+    SCRIPT_FILE="$TEMPLATE_DIR/$DISTRO-$TYPE.sh"
     cat $SCRIPT_FILE >> $KS_FILE
     echo "%end" >> $KS_FILE
 done
