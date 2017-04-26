22d46e0628efda0b0d004f915f1f26afd966ec26
Niclas Moeslund Overby <nimoov@prozum.dk>
2017-04-26 15:24:59 +0200

    master: generate and use account service key

--- global-conf.sh
+++ global-conf.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-export ETCD_SERVER="kube-master00"
-
+export ADMIN_EMAIL="noverb14@student.aau.dk"
+export MASTER_SERVER="kube-master00"
+export ETCD_SERVER="$MASTER_SERVER"
+export COCKPIT_SERVER="$MASTER_SERVER"
+export SERVICE_ACCOUNT_KEY="/etc/kubernetes/service-account.key"
